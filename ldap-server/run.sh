592b4f02f75627ea6ea06ca6cb2174287251f640
Lam Nguyen <hlam385@gmail.com>
2024-06-22 21:54:36 +0700

    update

--- ldap-server/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -d -p 389:389 --name openldap example/openldap:latest
--- sp-testapp/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -p 2080:80 -p 2443:443 --name="shibboleth-sp" example/shibboleth-sp:latest
